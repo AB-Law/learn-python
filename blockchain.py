ac78fd28435098fc012f59235935e996f233f0c1
TheLaw <akshay.law35@gmail.com>
2020-07-20 00:01:06 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -6,6 +6,8 @@ blockchain = []
 
 def get_last_value():
     #returns the last value of the blockchain
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 def add_value(transaction_amount , last_transaction=[1]):
@@ -16,16 +18,44 @@ def add_value(transaction_amount , last_transaction=[1]):
     last_transaction: the last transaction, with a default value of
     1, incase it's the first transaction in the blockchain
     '''
+    if last_transaction() == None:
+        last_transaction = [1]
     blockchain.append([last_transaction,transaction_amount])
     print(blockchain)
 
-def get_user_transaction():
+def get_transaction_value():
     '''User inputs the amount of cryptocurrency to transfer'''
     tx_amount = float(input('Amount of Lightnings to trasnfer: '))
     return tx_amount
 
+def display_blockchain():
+    for block in blockchain:
+        print('Current block')
+        print(block)
+
+def get_user_choice():
+    user_input = input("Your choice:")
+    return user_input
 
 
-add_value(get_user_transaction())
-add_value(get_user_transaction())
-add_value(get_user_transaction())
+
+
+while True:
+    print('Please choose')
+    print("1: Add a new transaction")
+    print("2.Display current block")
+    print("3.Exit loop")
+    user_choice = get_user_choice()
+    
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount,get_last_value())
+
+    elif user_choice == '2':
+        display_blockchain()
+
+    elif user_choice == '3':
+        break
+
+    else:
+        print('Input was valid, please pick a value from the list')
