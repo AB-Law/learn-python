ec4863c5721f5ebec94724150a2805a28abaa829
TheLaw <akshay.law35@gmail.com>
2020-07-20 00:32:30 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -18,7 +18,7 @@ def add_value(transaction_amount , last_transaction=[1]):
     last_transaction: the last transaction, with a default value of
     1, incase it's the first transaction in the blockchain
     '''
-    if last_transaction() == None:
+    if get_last_value() == None:
         last_transaction = [1]
     blockchain.append([last_transaction,transaction_amount])
     print(blockchain)
@@ -27,6 +27,21 @@ def get_transaction_value():
     '''User inputs the amount of cryptocurrency to transfer'''
     tx_amount = float(input('Amount of Lightnings to trasnfer: '))
     return tx_amount
+
+def verify_chain(): #Used to verify that the chain has not been tampered with
+    block_index = 0
+    is_valid = True
+    for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        elif block[0] == blockchain[block_index-1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+        block_index +=1
+    return is_valid
 
 def display_blockchain():
     for block in blockchain:
@@ -45,8 +60,9 @@ while True:
     print("1: Add a new transaction")
     print("2.Display current block")
     print("3.Exit loop")
+    print("4.Manipulate")
     user_choice = get_user_choice()
-    
+
     if user_choice == '1':
         tx_amount = get_transaction_value()
         add_value(tx_amount,get_last_value())
@@ -57,5 +73,12 @@ while True:
     elif user_choice == '3':
         break
 
+    elif user_choice == '4':
+        if len(blockchain)>=1:
+            blockchain[0] = [2]
+
     else:
         print('Input was valid, please pick a value from the list')
+    if not verify_chain():
+        print("Invalid blockchain")
+        break
