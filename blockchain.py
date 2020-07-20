67a0597f2368ec5e7376a273cb7d045cfa9e262c
TheLaw <akshay.law35@gmail.com>
2020-07-20 14:06:03 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -29,24 +29,35 @@ def get_transaction_value():
     return tx_amount
 
 def verify_chain(): #Used to verify that the chain has not been tampered with
-    block_index = 0
     is_valid = True
-    for block in blockchain:
+    for block_index in range(len(blockchain)):
+        #checking to see if it is the first element
         if block_index == 0:
-            block_index += 1
             continue
-        elif block[0] == blockchain[block_index-1]:
+
+'''Blockchain[block_index] gives us the list of the current index
+, and we check the first element which should be the list
+of the last chain, ie blockchain[block_index-1].
+eg.
+to check [[[1],2],3] with [[1],2]
+so we check the first element of the currnt indext and compare
+with the previous one
+     '''
+
+        elif blockchain[block_index][0] == blockchain[block_index-1]:
             is_valid = True
         else:
             is_valid = False
             break
-        block_index +=1
+
     return is_valid
 
 def display_blockchain():
     for block in blockchain:
         print('Current block')
         print(block)
+    else:
+        print('-' * 20)
 
 def get_user_choice():
     user_input = input("Your choice:")
@@ -54,8 +65,9 @@ def get_user_choice():
 
 
 
+wait_input = True
 
-while True:
+while wait_input:
     print('Please choose')
     print("1: Add a new transaction")
     print("2.Display current block")
@@ -65,20 +77,24 @@ while True:
 
     if user_choice == '1':
         tx_amount = get_transaction_value()
-        add_value(tx_amount,get_last_value())
+        add_value(tx_amount, get_last_value())
 
     elif user_choice == '2':
         display_blockchain()
 
     elif user_choice == '3':
-        break
+        wait_input = False
 
     elif user_choice == '4':
-        if len(blockchain)>=1:
+        if len(blockchain) >= 1:
             blockchain[0] = [2]
 
     else:
         print('Input was valid, please pick a value from the list')
     if not verify_chain():
         print("Invalid blockchain")
+        display_blockchain()
         break
+
+else:
+    print('User has stopped')
